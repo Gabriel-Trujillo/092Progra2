e13c735ac2665219b4d1fa453be0c8e3decd8e99
Gabriel-Trujillo <98632737+Gabriel-Trujillo@users.noreply.github.com>
2022-02-20 04:50:28 -0600

    Add files via upload

---
+++ 201807349_TAREAP1/201807349_CHUMPAS/201807349_CHUMPAS/Form1.Designer.vb
@@ -0,0 +1,314 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnAlgodon = New System.Windows.Forms.RadioButton()
+        Me.btnSeda = New System.Windows.Forms.RadioButton()
+        Me.btnLona = New System.Windows.Forms.RadioButton()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblPCosto = New System.Windows.Forms.Label()
+        Me.lblTCosto = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblPVenta = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btnLarge = New System.Windows.Forms.RadioButton()
+        Me.btnMedium = New System.Windows.Forms.RadioButton()
+        Me.btnSmall = New System.Windows.Forms.RadioButton()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 5)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(118, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Material: (yarda)"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 4)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Tamaño:"
+        '
+        'btnAlgodon
+        '
+        Me.btnAlgodon.AutoSize = True
+        Me.btnAlgodon.Location = New System.Drawing.Point(35, 37)
+        Me.btnAlgodon.Name = "btnAlgodon"
+        Me.btnAlgodon.Size = New System.Drawing.Size(143, 24)
+        Me.btnAlgodon.TabIndex = 8
+        Me.btnAlgodon.TabStop = True
+        Me.btnAlgodon.Text = "Algodón       Q15"
+        Me.btnAlgodon.UseVisualStyleBackColor = True
+        '
+        'btnSeda
+        '
+        Me.btnSeda.AutoSize = True
+        Me.btnSeda.Location = New System.Drawing.Point(35, 67)
+        Me.btnSeda.Name = "btnSeda"
+        Me.btnSeda.Size = New System.Drawing.Size(161, 24)
+        Me.btnSeda.TabIndex = 9
+        Me.btnSeda.TabStop = True
+        Me.btnSeda.Text = "Seda             Q23.99"
+        Me.btnSeda.UseVisualStyleBackColor = True
+        '
+        'btnLona
+        '
+        Me.btnLona.AutoSize = True
+        Me.btnLona.Location = New System.Drawing.Point(35, 97)
+        Me.btnLona.Name = "btnLona"
+        Me.btnLona.Size = New System.Drawing.Size(160, 24)
+        Me.btnLona.TabIndex = 10
+        Me.btnLona.TabStop = True
+        Me.btnLona.Text = "Lona             Q30.99"
+        Me.btnLona.UseVisualStyleBackColor = True
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(73, 202)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(94, 29)
+        Me.btnCalc.TabIndex = 11
+        Me.btnCalc.Text = "Calcular"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(234, 211)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(116, 20)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Precio de Costo:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(234, 271)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(108, 20)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Total de Costo:"
+        '
+        'lblPCosto
+        '
+        Me.lblPCosto.AutoSize = True
+        Me.lblPCosto.Location = New System.Drawing.Point(356, 211)
+        Me.lblPCosto.Name = "lblPCosto"
+        Me.lblPCosto.Size = New System.Drawing.Size(0, 20)
+        Me.lblPCosto.TabIndex = 14
+        '
+        'lblTCosto
+        '
+        Me.lblTCosto.AutoSize = True
+        Me.lblTCosto.Location = New System.Drawing.Point(356, 271)
+        Me.lblTCosto.Name = "lblTCosto"
+        Me.lblTCosto.Size = New System.Drawing.Size(0, 20)
+        Me.lblTCosto.TabIndex = 15
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(640, 282)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(57, 29)
+        Me.Button1.TabIndex = 16
+        Me.Button1.Text = "Salir"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(234, 241)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(115, 20)
+        Me.Label5.TabIndex = 17
+        Me.Label5.Text = "Precio de Venta:"
+        '
+        'lblPVenta
+        '
+        Me.lblPVenta.AutoSize = True
+        Me.lblPVenta.Location = New System.Drawing.Point(355, 241)
+        Me.lblPVenta.Name = "lblPVenta"
+        Me.lblPVenta.Size = New System.Drawing.Size(0, 20)
+        Me.lblPVenta.TabIndex = 18
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.btnLarge)
+        Me.Panel1.Controls.Add(Me.btnMedium)
+        Me.Panel1.Controls.Add(Me.btnSmall)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(204, 137)
+        Me.Panel1.TabIndex = 23
+        '
+        'btnLarge
+        '
+        Me.btnLarge.AutoSize = True
+        Me.btnLarge.Location = New System.Drawing.Point(32, 91)
+        Me.btnLarge.Name = "btnLarge"
+        Me.btnLarge.Size = New System.Drawing.Size(153, 24)
+        Me.btnLarge.TabIndex = 7
+        Me.btnLarge.TabStop = True
+        Me.btnLarge.Text = "Large          Q99.99"
+        Me.btnLarge.UseVisualStyleBackColor = True
+        '
+        'btnMedium
+        '
+        Me.btnMedium.AutoSize = True
+        Me.btnMedium.Location = New System.Drawing.Point(32, 61)
+        Me.btnMedium.Name = "btnMedium"
+        Me.btnMedium.Size = New System.Drawing.Size(151, 24)
+        Me.btnMedium.TabIndex = 6
+        Me.btnMedium.TabStop = True
+        Me.btnMedium.Text = "Medium     Q85.99"
+        Me.btnMedium.UseVisualStyleBackColor = True
+        '
+        'btnSmall
+        '
+        Me.btnSmall.AutoSize = True
+        Me.btnSmall.Location = New System.Drawing.Point(32, 31)
+        Me.btnSmall.Name = "btnSmall"
+        Me.btnSmall.Size = New System.Drawing.Size(153, 24)
+        Me.btnSmall.TabIndex = 5
+        Me.btnSmall.TabStop = True
+        Me.btnSmall.Text = "Small          Q65.50"
+        Me.btnSmall.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.Label1)
+        Me.Panel2.Controls.Add(Me.btnAlgodon)
+        Me.Panel2.Controls.Add(Me.btnSeda)
+        Me.Panel2.Controls.Add(Me.btnLona)
+        Me.Panel2.Location = New System.Drawing.Point(236, 10)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(218, 139)
+        Me.Panel2.TabIndex = 24
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.txtCantidad)
+        Me.Panel3.Controls.Add(Me.Label7)
+        Me.Panel3.Location = New System.Drawing.Point(477, 12)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(200, 137)
+        Me.Panel3.TabIndex = 25
+        '
+        'txtCantidad
+        '
+        Me.txtCantidad.Location = New System.Drawing.Point(43, 60)
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(125, 27)
+        Me.txtCantidad.TabIndex = 1
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(15, 5)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(72, 20)
+        Me.Label7.TabIndex = 0
+        Me.Label7.Text = "Cantidad:"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(73, 241)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(94, 29)
+        Me.Button2.TabIndex = 26
+        Me.Button2.Text = "Limpiar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Info
+        Me.ClientSize = New System.Drawing.Size(709, 323)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.lblPVenta)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblTCosto)
+        Me.Controls.Add(Me.lblPCosto)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Name = "Form1"
+        Me.Text = "Chumpas "
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnAlgodon As RadioButton
+    Friend WithEvents btnSeda As RadioButton
+    Friend WithEvents btnLona As RadioButton
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblPCosto As Label
+    Friend WithEvents lblTCosto As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lblPVenta As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents btnSmall As RadioButton
+    Friend WithEvents btnLarge As RadioButton
+    Friend WithEvents btnMedium As RadioButton
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents txtCantidad As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Button2 As Button
+End Class
---
+++ 201807349_TAREAP1/201807349_CHUMPAS/201807349_CHUMPAS/Form1.vb
@@ -0,0 +1,83 @@
+﻿Public Class Form1
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+
+        If btnSmall.Checked() Or btnMedium.Checked() Or btnLarge.Checked() Then
+
+            If btnAlgodon.Checked() Or btnSeda.Checked() Or btnLona.Checked() Then
+
+                If txtCantidad.Text() = "" Then
+
+                    MsgBox("Debe ingresar una cantidad", 64, "Ingrese una cantidad")
+
+                Else
+
+                    Dim material, precioCosto, precioVenta As Double
+
+                    If btnAlgodon.Checked() Then
+                        material = 15
+                    ElseIf btnSeda.Checked() Then
+                        material = 23.99
+                    ElseIf btnLona.Checked() Then
+                        material = 30.99
+                    End If
+
+                    If btnSmall.Checked() Then
+                        precioCosto = 65.5 + 2 * material * Val(txtCantidad.Text())
+                    End If
+
+                    If btnMedium.Checked() Then
+                        precioCosto = 85.99 + 2.5 * material * Val(txtCantidad.Text())
+                    End If
+
+                    If btnLarge.Checked() Then
+                        precioCosto = 30.99 + 3 * material * Val(txtCantidad.Text())
+                    End If
+
+                    precioVenta = precioCosto + 0.65 * precioCosto
+
+                    lblPCosto.Text() = Math.Round(precioCosto, 2)
+                    lblPVenta.Text() = Math.Round(precioVenta, 2)
+                    lblTCosto.Text() = Math.Round(precioCosto + precioVenta, 2)
+
+                End If
+
+            Else
+                MsgBox("Debe seleccionar un tipo de material", 64, "Escoge un material")
+            End If
+
+        Else
+            MsgBox("Debe seleccionar al menos un tamaño", 64, "Escoge un tamaño")
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim meboi As DialogResult
+
+        meboi = MsgBox("¿De verdad me vas a dejar?", 4, "No te vayas :(")
+
+        Console.WriteLine(meboi)
+
+        If meboi = DialogResult.Yes Then
+            Me.Close()
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        txtCantidad.Clear()
+        btnSmall.Checked = False
+        btnMedium.Checked = False
+        btnLarge.Checked = False
+        btnAlgodon.Checked = False
+        btnSeda.Checked = False
+        btnLona.Checked = False
+        txtCantidad.Text = 0
+
+        lblPCosto.Text = 0
+        lblPVenta.Text = 0
+        lblTCosto.Text = 0
+
+
+    End Sub
+End Class
