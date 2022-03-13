d8f717b3f0dd64904b199021b457f6f350fff7ad
Gabriel-Trujillo <98632737+Gabriel-Trujillo@users.noreply.github.com>
2022-03-12 20:48:29 -0600

    Add files via upload

---
+++ Tarea5/Tarea5/Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class Form1
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        If indice < 8 Then
+                matriz(0, indice) = TextCarne.Text
+                matriz(1, indice) = TextNombre.Text
+                matriz(2, indice) = ComboIdioma.SelectedItem
+                matriz(3, indice) = DateInscripccion.Value
+                matriz(4, indice) = ComboDias.SelectedItem
+                matriz(5, indice) = TextHoras.Text
+
+                If RadioButtonCheque.Checked Then
+                    matriz(6, indice) = RadioButtonCheque.Text
+                ElseIf RadioButtonEfectivo.Checked Then
+                    matriz(6, indice) = RadioButtonEfectivo.Text
+                End If
+
+            Select Case ComboIdioma.SelectedIndex
+                Case 0 : matriz(7, indice) = Str(Val(matriz(5, indice) * Ingles))
+                Case 1 : matriz(7, indice) = Str(Val(matriz(5, indice) * Portugues))
+                Case 2 : matriz(7, indice) = Str(Val(matriz(5, indice) * Frances))
+            End Select
+
+
+
+            If RadioButtonEfectivo.Checked Then
+                matriz(8, indice) = Str(Val(matriz(7, indice) * DescEfectivo))
+
+            End If
+
+            If RadioButtonCheque.Checked Then
+                matriz(8, indice) = Str(Val(matriz(7, indice) * DescCheque))
+
+            End If
+
+            Select Case ComboDias.SelectedIndex
+                Case 2 : matriz(9, indice) = Str(Val(matriz(7, indice) * DescDias))
+            End Select
+
+
+
+            matriz(10, indice) = ((Str(Val(matriz(7, indice)) - Val(matriz(8, indice)) - Val(matriz(9, indice)))))
+
+
+            DataGridView1.Rows.Add(indice + 1, matriz(0, indice), matriz(1, indice), matriz(2, indice), matriz(3, indice), matriz(4, indice), matriz(5, indice), matriz(6, indice), matriz(7, indice), matriz(8, indice), matriz(9, indice), matriz(10, indice))
+            indice = indice + 1
+
+            Else
+                MessageBox.Show("No hay más espacio")
+
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If MsgBox("¿Desea salir?", vbYesNo) - vbYes Then
+            End
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        LimpiarDatosEntrada()
+    End Sub
+End Class
---
+++ Tarea5/Tarea5/Module1.vb
@@ -0,0 +1,43 @@
+﻿Module Module1
+
+    Public matriz(10, 7)
+    Public indice = 0
+
+    Public Const Ingles = 150
+    Public Const Portugues = 80
+    Public Const Frances = 125
+    Public Const DescEfectivo = 0.02
+    Public Const DescCheque = 0.015
+    Public Const DescDias = 0.05
+
+    Sub LimpiarDatosEntrada()
+        Form1.TextCarne.Clear()
+        Form1.TextNombre.Clear()
+        Form1.TextHoras.Clear()
+        Form1.RadioButtonCheque.Checked = False
+        Form1.RadioButtonEfectivo.Checked = False
+
+
+        Form1.ComboDias.SelectedItem = Nothing
+        Form1.ComboIdioma.SelectedItem = Nothing
+
+
+    End Sub
+    Sub LimpiarResultados()
+        matriz(0, indice) = 0
+        matriz(1, indice) = 0
+        matriz(2, indice) = 0
+        matriz(3, indice) = 0
+        matriz(4, indice) = 0
+        matriz(5, indice) = 0
+        matriz(6, indice) = 0
+        matriz(7, indice) = 0
+        matriz(8, indice) = 0
+        matriz(9, indice) = 0
+        matriz(10, indice) = 0
+
+        Form1.DataGridView1.Rows.Clear()
+    End Sub
+
+
+End Module
