7b05bbd66e6c8debad3b52b83d318e786952a399
Gabriel-Trujillo <98632737+Gabriel-Trujillo@users.noreply.github.com>
2022-04-21 18:47:15 -0600

    Add files via upload

---
+++ 201807349_CORTO3/201807349_HOTEL/Form1.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.CálculoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MostrarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ConsultarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EliminarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LimpiarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(33, 55)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(243, 145)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Datos"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(94, 94)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox3.TabIndex = 5
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(94, 61)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(94, 28)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(14, 94)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Días"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(14, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(32, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "NIT"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(14, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Sencilla", "Doble", "Cabaña"})
+        Me.ComboBox1.Location = New System.Drawing.Point(295, 65)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(151, 28)
+        Me.ComboBox1.TabIndex = 2
+        Me.ComboBox1.Text = "Tipo habitación"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CálculoToolStripMenuItem, Me.MostrarToolStripMenuItem, Me.ConsultarToolStripMenuItem, Me.EliminarToolStripMenuItem, Me.LimpiarToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(868, 28)
+        Me.MenuStrip1.TabIndex = 3
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'CálculoToolStripMenuItem
+        '
+        Me.CálculoToolStripMenuItem.Name = "CálculoToolStripMenuItem"
+        Me.CálculoToolStripMenuItem.Size = New System.Drawing.Size(72, 24)
+        Me.CálculoToolStripMenuItem.Text = "Cálculo"
+        '
+        'MostrarToolStripMenuItem
+        '
+        Me.MostrarToolStripMenuItem.Name = "MostrarToolStripMenuItem"
+        Me.MostrarToolStripMenuItem.Size = New System.Drawing.Size(74, 24)
+        Me.MostrarToolStripMenuItem.Text = "Mostrar"
+        '
+        'ConsultarToolStripMenuItem
+        '
+        Me.ConsultarToolStripMenuItem.Name = "ConsultarToolStripMenuItem"
+        Me.ConsultarToolStripMenuItem.Size = New System.Drawing.Size(85, 24)
+        Me.ConsultarToolStripMenuItem.Text = "Consultar"
+        '
+        'EliminarToolStripMenuItem
+        '
+        Me.EliminarToolStripMenuItem.Name = "EliminarToolStripMenuItem"
+        Me.EliminarToolStripMenuItem.Size = New System.Drawing.Size(77, 24)
+        Me.EliminarToolStripMenuItem.Text = "Eliminar"
+        '
+        'LimpiarToolStripMenuItem
+        '
+        Me.LimpiarToolStripMenuItem.Name = "LimpiarToolStripMenuItem"
+        Me.LimpiarToolStripMenuItem.Size = New System.Drawing.Size(73, 24)
+        Me.LimpiarToolStripMenuItem.Text = "Limpiar"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(33, 238)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 29
+        Me.DataGridView1.Size = New System.Drawing.Size(648, 188)
+        Me.DataGridView1.TabIndex = 4
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"Efectivo", "Tarjeta", "Transferencia", "Depósito"})
+        Me.ComboBox2.Location = New System.Drawing.Point(295, 99)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(151, 28)
+        Me.ComboBox2.TabIndex = 5
+        Me.ComboBox2.Text = "Tipo pago"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(868, 496)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents CálculoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MostrarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ConsultarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EliminarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LimpiarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents ComboBox2 As ComboBox
+End Class
---
+++ 201807349_CORTO3/201807349_HOTEL/Form1.vb
@@ -0,0 +1,75 @@
+﻿Public Class Form1
+
+    Public Const PrecioSencilla = 250
+    Public Const PrecioDoble = 400
+    Public Const PrecioCabaña = 650
+
+    Public Const DescEfectivo = 0.15
+    Public Const RecTarjeta = 0.03
+
+
+    Public contador = 0
+    Public Nombrecliente(7)
+    Public Días(7)
+    Public TipoHabitacion(7)
+    Public Tipopago(7)
+
+    Sub Limpiar()
+
+        Public Nombrecliente(7) = 0
+        Public Días(7) = 0
+        Public TipoHabitacion(7) = 0
+        Public Tipopago(7) = 0
+    End Sub
+
+
+
+    Private Sub CálculoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CálculoToolStripMenuItem.Click
+
+        Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If ComboBox1.SelectedIndex = -1 Or ComboBox2.SelectedIndex = -1 Then
+            MsgBox("No seleccionó todos los datos")
+            Exit Sub
+
+        End If
+        If contador < 8 Then
+
+            numeroventa(contador) = contador + 1
+
+            tamaño(contador) = ComboBox1.SelectedItem
+            material(contador) = ComboBox2.SelectedItem
+
+
+            Select Case tamaño(contador)
+                Case "Sofá"
+                    Select Case material(contador)
+                        Case "Sencilla"
+                            (contador) = Psencilla
+                            preciocosto(contador) = 8 * Psencilla + DescEfectivo(contador)
+                        Case "Doble"
+                    End Select
+                Case "Doble"
+                    Select Case Doble(contador)
+                        Case "Doble"
+                            PrecioDoble(contador) = Pdoble
+                            preciocosto(contador) = 8 * Pdoble + RecTarjeta(contador)
+                        Case "Doble"
+                    End Select
+
+                Case "Cabaña"
+                    Select Case Tipopago(contador)
+                        Case "Efectivo"
+                            PrecioCabaña(contador) = PrecioCabaña
+                            PrecioCabaña(contador) = 8 * PrecioCabaña + RecTarjeta(contador)
+                        Case "Cuerina"
+                    End Select
+
+            End Select
+
+
+    End Sub
+
+    Private Sub LimpiarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LimpiarToolStripMenuItem.Click
+        Limpiar()
+    End Sub
+End Class
